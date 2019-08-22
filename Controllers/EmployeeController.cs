374547faa37c175a183fe6f58a5a8c419be68134
Abu-Maestro <engryusufmayowa@gmail.com>
2019-08-22 09:19:31 +0100

    First Commit:

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using EmployeeManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeDbContext _db;
+        public EmployeeController()
+        {
+            _db = new EmployeeDbContext();
+        }
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employees = _db.Employees.ToList();
+            return View(employees);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Employees.Add(model);
+                _db.SaveChanges();
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ EmployeeDbContext - Copy.cs
@@ -0,0 +1,27 @@
+﻿using EmployeeManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace EmployeeManagementSystem
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext() : base("name=employeedb")
+        {
+            Database.Initialize(false);
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            modelBuilder.Configurations.AddFromAssembly(currentAssembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
